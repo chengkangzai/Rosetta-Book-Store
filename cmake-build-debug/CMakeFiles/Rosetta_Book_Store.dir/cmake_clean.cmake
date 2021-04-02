c56b72d0e9866c30bac4ae0eb46fa7edf2dfda2a
chengkangzai <pycck@hotmail.com>
2021-04-02 18:00:27 +0800

    Init Project
    Signed-off-by: chengkangzai <pycck@hotmail.com>

---
+++ cmake-build-debug/CMakeFiles/Rosetta_Book_Store.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Rosetta_Book_Store.dir/main.cpp.obj"
+  "Rosetta_Book_Store.exe"
+  "Rosetta_Book_Store.exe.manifest"
+  "Rosetta_Book_Store.lib"
+  "Rosetta_Book_Store.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Rosetta_Book_Store.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Rosetta_Book_Store.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Rosetta_Book_Store.
+# This may be replaced when dependencies are built.
