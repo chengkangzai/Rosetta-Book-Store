b8b533e6f87144dedc3ed2474776b8f9da9c7f38
chengkangzai <pycck@hotmail.com>
2021-04-03 13:03:43 +0800

    add Book Query

---
+++ Exception/ModalNotFoundException.h
@@ -0,0 +1,19 @@
+//
+// Created by CCK on 2021-04-03.
+//
+
+#ifndef ROSETTA_BOOK_STORE_MODALNOTFOUNDEXCEPTION_H
+#define ROSETTA_BOOK_STORE_MODALNOTFOUNDEXCEPTION_H
+
+#include <iostream>
+
+class ModalNotFoundException : std::exception {
+
+public:
+    ModalNotFoundException(const char *msg) : std::exception(msg) {
+    }
+
+};
+
+
+#endif //ROSETTA_BOOK_STORE_MODALNOTFOUNDEXCEPTION_H
--- Query/BookQuery.h
+++ Query/BookQuery.h
@@ -9,11 +9,16 @@
 #include <utility>
 
 #include "../Models/Book.h"
+#include "../Exception/ModalNotFoundException.h"
 
 using namespace std;
 
 class BookQuery {
 public:
+    enum QUERY_TYPE {
+        TITLE, ID, AUTHOR, INDEX
+    };
+
     struct BooksNode {
         explicit BooksNode(Book book) : book(move(book)) {
         }
@@ -24,15 +29,39 @@ public:
 
     struct BooksNode *head = nullptr;
 
-    Book get(int index) const {
-        if (index < 0 || index >= size()) {
-            throw exception("Index out of bound. \n");
-        }
-        BooksNode *current = head;
-        for (int i = 0; i < index; i++)
-            current = current->next;
 
-        return current->book;
+    /**
+     * @param TYPE Search type. Available query : TITLE,ID,AUTHOR,INDEX
+     * @param searchQuery
+     * @return
+     */
+    Book where(QUERY_TYPE TYPE, string searchQuery) {
+        if (TYPE == TITLE)
+            return this->searchByTitle(searchQuery);
+        if (TYPE == ID)
+            return this->searchByID(stoi(searchQuery));
+        if (TYPE == AUTHOR)
+            return this->searchByAuthor(searchQuery);
+        if (TYPE == INDEX)
+            return this->searchByIndex(stoi(searchQuery));
+
+        throw exception("Invalid QUERY_TYPE !");
+    }
+
+    /**
+     * @param TYPE Search type. Available query : ID,INDEX
+     * @param searchQuery
+     * @return
+     */
+    Book where(QUERY_TYPE TYPE, int searchQuery) {
+        if (TYPE == ID)
+            return this->searchByID(searchQuery);
+        if (TYPE == INDEX)
+            return this->searchByIndex(searchQuery);
+        if (TYPE == AUTHOR || TYPE == TITLE) {
+            throw exception("QUERY_TYPE not supported ! ps: Author and TITLE with int...?");
+        }
+        throw exception("Invalid QUERY_TYPE !");
     }
 
     BookQuery *create(Book book) {
@@ -44,13 +73,11 @@ public:
             return this;
         }
 
-        BooksNode *last = head;
-        while (last->next != nullptr) {
-            last = last->next;
+        BooksNode *current = head;
+        while (current->next != nullptr) {
+            current = current->next;
         }
-        last->next = newNode;
-
-        delete newNode;
+        current->next = newNode;
         return this;
     }
 
@@ -105,9 +132,7 @@ public:
     BookQuery *printAll() {
         BooksNode *current = head;
 
-        for (int i = 0; i < size(); i++) {
-            if (current == nullptr)
-                return this;
+        while (current != nullptr) {
             cout << current->book.toString() << "\n";
             current = current->next;
         }
@@ -116,33 +141,41 @@ public:
     }
 
 
+
     BookQuery *init() {
-        this->create(Book(1, "HARRY POTTER", "J. K. ROWLING", "FANTASY", "FICTION", "9780747532743", 50, 56.80, true));
-        this->create(Book(2, "MEIN KAMPF", "ADOLF HITLER", "HISTORICAL", "FICTION", "9780395925034", 5, 120.50, false));
-        this->create(Book(3, "13 REASONS WHY", "JAY ASHER", "REALISTIC", "FICTION", "9780451479327", 3, 63.2, true));
         this->create(
-                Book(4, "THE INVISIBLE MAN", "H. G. WELLS", "SCIENCE", "FICTION", "9781512091977", 5, 30.45, true));
+                Book(1, "HARRY POTTER", "J. K. ROWLING", "FANTASY", "FICTION",
+                     "9780747532743", 50, 56.80, true));
         this->create(
-                Book(5, "THE HUNGER GAMES", "SUZANNE COLLINS", "NARRATIVE", "NON-FICTION", "9780439023481", 9, 124.41,
-                     true));
+                Book(2, "MEIN KAMPF", "ADOLF HITLER", "HISTORICAL", "FICTION",
+                     "9780395925034", 5, 120.50, false));
         this->create(
-                Book(6, "I HAVE A DREAM", "MARTIN LUTHER KING JR.", "BIOGRAPHY", "NON-FICTION", "9780375858871", 30,
-                     100, true));
+                Book(3, "13 REASONS WHY", "JAY ASHER", "REALISTIC", "FICTION",
+                     "9780451479327", 3, 63.2, true));
         this->create(
-                Book(7, "CRIME AND PERIODICALS", "NOVA EVERLY", "PERIODICALS", "NON-FICTION", "9781949202076", 2, 35.9,
-                     true));
+                Book(4, "THE INVISIBLE MAN", "H. G. WELLS", "SCIENCE", "FICTION",
+                     "9781512091977", 5, 30.45, true));
         this->create(
-                Book(8, "A NEW EARTH", "ECKHART TOLLE", "SELF-HELP", "NON-FICTION", "9780452289963", 10, 15, true));
+                Book(5, "THE HUNGER GAMES", "SUZANNE COLLINS", "NARRATIVE", "NON-FICTION",
+                     "9780439023481", 9, 124.41, true));
         this->create(
-                Book(9, "THE HERO WITH A THOUSAND FACES", "JOSEPH CAMPBELL", "MYTH", "NON-FICTION", "9781577315933", 6,
-                     78, true));
+                Book(6, "I HAVE A DREAM", "MARTIN LUTHER KING JR.", "BIOGRAPHY", "NON-FICTION",
+                     "9780375858871", 30, 100, true));
+        this->create(
+                Book(7, "CRIME AND PERIODICALS", "NOVA EVERLY", "PERIODICALS", "NON-FICTION",
+                     "9781949202076", 2, 35.9, true));
+        this->create(
+                Book(8, "A NEW EARTH", "ECKHART TOLLE", "SELF-HELP", "NON-FICTION",
+                     "9780452289963", 10, 15, true));
+        this->create(
+                Book(9, "THE HERO WITH A THOUSAND FACES", "JOSEPH CAMPBELL", "REFERENCE", "NON-FICTION",
+                     "9781577315933", 6, 78, true));
         return this;
     }
 
-//private:
+private:
     int size() const {
-        struct BooksNode *ptr;
-        ptr = head;
+        struct BooksNode *ptr = head;
         int temp = 0;
 
         while (ptr != nullptr) {
@@ -154,6 +187,59 @@ public:
 
     void commit() {
         // TODO
+    }
+
+    Book searchByTitle(const string &title) const {
+        struct BooksNode *current = head;
+
+        while (current != nullptr) {
+            if (current->book.title == title) {
+                return current->book;
+            } else {
+                current = current->next;
+            }
+        }
+
+        throw ModalNotFoundException("Modal (Book) with search query of: title");
+    }
+
+    Book searchByAuthor(const string &author) const {
+        struct BooksNode *current = head;
+
+        while (current != nullptr) {
+            if (current->book.author == author) {
+                return current->book;
+            } else {
+                current = current->next;
+            }
+        }
+
+        throw ModalNotFoundException("Modal (Book) with search query of: author");
+    }
+
+    Book searchByID(int id) const {
+        struct BooksNode *current = head;
+
+        while (current != nullptr) {
+            if (current->book.id == id) {
+                return current->book;
+            } else {
+                current = current->next;
+            }
+        }
+
+        throw ModalNotFoundException("Modal (Book) with search query of: id");
+    }
+
+    Book searchByIndex(int index) const {
+        if (index < 0 || index >= size()) {
+            throw exception("Index out of bound. \n");
+        }
+        BooksNode *current = head;
+        for (int i = 0; i < index; i++)
+            current = current->next;
+
+        return current->book;
     }
 };
 
