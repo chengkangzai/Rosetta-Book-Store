a166fd42ce7686e14e2727d07d93a0505a01df04
Ching Cheng Kang <pycck@hotmail.com>
2021-04-24 21:49:01 +0800

    Custom exception (#4)
    * fix exception wont work in mac os
    
    * fix c++ docs

--- Exception/InvalidQueryType.h
+++ Exception/InvalidQueryType.h
@@ -7,11 +7,35 @@
 
 #include <iostream>
 
-class InvalidQueryType : public std::exception {
+class InvalidQueryType : virtual public std::exception {
+
+protected:
+    std::string error_message;      ///< Error message
+
 public:
-    InvalidQueryType(const char *msg) : std::exception(msg) {
+
+    /** Constructor (C++ STL string, int, int).
+     *  @param msg The error message
+     */
+    explicit
+    InvalidQueryType(const std::string &msg) :
+            error_message(msg) {}
+
+    /** Destructor.
+     *  Virtual to allow for subclassing.
+     */
+    virtual ~InvalidQueryType() throw() {}
+
+    /** Returns a pointer to the (constant) error description.
+     *  @return A pointer to a const char*. The underlying memory
+     *  is in possession of the Except object. Callers must
+     *  not attempt to free the memory.
+     */
+    virtual const char *what() const throw() {
+        return error_message.c_str();
     }
 };
+
 
 
 #endif //ROSETTA_BOOK_STORE_INVALIDQUERYTYPE_H
--- Exception/ModalNotFoundException.h
+++ Exception/ModalNotFoundException.h
@@ -7,12 +7,34 @@
 
 #include <iostream>
 
-class ModalNotFoundException : public std::exception {
+//https://riptutorial.com/cplusplus/example/23640/custom-exception
+class ModalNotFoundException : virtual public std::exception {
+
+protected:
+    std::string error_message;      ///< Error message
 
 public:
-    ModalNotFoundException(const char *msg) : std::exception(msg) {
-    }
 
+    /** Constructor (C++ STL string, int, int).
+     *  @param msg The error message
+     */
+    explicit
+    ModalNotFoundException(const std::string &msg) :
+            error_message(msg) {}
+
+    /** Destructor.
+     *  Virtual to allow for subclassing.
+     */
+    virtual ~ModalNotFoundException() throw() {}
+
+    /** Returns a pointer to the (constant) error description.
+     *  @return A pointer to a const char*. The underlying memory
+     *  is in possession of the Except object. Callers must
+     *  not attempt to free the memory.
+     */
+    virtual const char *what() const throw() {
+        return error_message.c_str();
+    }
 };
 
 
